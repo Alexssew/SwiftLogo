3b2fa6b36f8d9e5e1d748c9baa282ab7f7ea4b34
AlexS <siralexx50@gmai.com>
2022-04-15 14:20:30 +0300

    Initial Commit

---
+++ SwiftLogo/SwiftLogoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLogoApp.swift
+//  SwiftLogo
+//
+//  Created by Alex on 15/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLogoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
